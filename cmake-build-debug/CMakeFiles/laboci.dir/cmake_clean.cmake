a6281bede2ce00ce632ce79671c5e03c93f46b10
alexndr03 <nag.alexandr03@yandex.ru>
2021-02-10 17:48:32 +0300

    Добавил программу 1

---
+++ cmake-build-debug/CMakeFiles/laboci.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laboci.dir/main.cpp.obj"
+  "laboci.exe"
+  "laboci.exe.manifest"
+  "laboci.lib"
+  "laboci.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laboci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
