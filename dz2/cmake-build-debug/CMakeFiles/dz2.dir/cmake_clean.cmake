aeae320cd0eaf108cfbe11df8534072cab08e5d9
alexndr03 <nag.alexandr03@yandex.ru>
2021-02-14 20:09:01 +0300

    не делаю

---
+++ dz2/cmake-build-debug/CMakeFiles/dz2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dz2.dir/main.cpp.obj"
+  "dz2.exe"
+  "dz2.exe.manifest"
+  "dz2.lib"
+  "dz2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dz2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ dz2/cmake-build-debug/CMakeFiles/dz2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dz2.
+# This may be replaced when dependencies are built.
