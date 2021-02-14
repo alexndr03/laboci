515a756be20f2d4d807b1407ff1dab3f2d651301
alexndr03 <nag.alexandr03@yandex.ru>
2021-02-14 16:36:49 +0300

    что-то делаю

---
+++ dz1/cmake-build-debug-visual-studio/CMakeFiles/dz1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dz1.dir/main.cpp.obj"
+  "dz1.exe"
+  "dz1.exe.manifest"
+  "dz1.lib"
+  "dz1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dz1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
